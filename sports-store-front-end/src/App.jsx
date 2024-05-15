b7c26220248b2b5ba10ce66abd85daf9a2268dc9
GajendraKaushik <gajendrakaushik128@gmail.com>
2024-05-15 08:46:56 +0530

    UI-Update: General Change

--- sports-store-front-end/src/App.jsx
+++ sports-store-front-end/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
       <NavBar/>
       <ProductsList />
-      <ImageSlider />
+      <ImageSlider/>
        
 
     </>
--- sports-store-front-end/src/assets/components/ProductsList.jsx
+++ sports-store-front-end/src/assets/components/ProductsList.jsx
@@ -8,6 +8,7 @@ import Accessa  from "../images/Accesaries.webp"
 import apparel  from "../images/apparel.webp"
 import kids  from "../images/kids_bike.webp"
 import tire  from "../images/Tire.webp"
+import Homeimg from "../images/HomePage.webp"
 
 import Button from "./Button";
 const ProductsList = () =>{
@@ -41,33 +42,13 @@ const ProductsList = () =>{
 
    return(
     <>
-        {/* <div className="w-56 h-52 my-20 mx-6">
-            <div className="relative overflow-hidden rounded-lg ">
-            <img src={Tileimg} alt="img" className="w-full h-full object-cover transition-transform group-hover:scale-110 duration-300"/>
-            
-            <div className="absolute p-6 w-full h-full flex gap-7 flex-col items-center justify-center ">
-                <div className="text-slate-50 font-bold"> Electric Bick </div>
-                <button  className="bg-white w-20 h-10 rounded-lg font-bold text-sm"> Shop </button>
+        <div className="LandinImageContainer">
+            <div className="w-full h-[810px] top-20 relative">
+            <img src={Homeimg} alt="HomeImg" className="w-full h-full z-50 transform " />
             </div>
-           
-
-            </div>
-        </div> */}
-
-{/* <div className="w-56 h-52 my-20 mx-6 md:w-80 md:h-64">
-    <div className="relative overflow-hidden rounded-lg group">
-        <img
-            src={Tileimg} 
-            alt="Electric Bike"
-            className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
-        />  
-      
-        <div className="absolute inset-0 p-6 flex flex-col items-center gap-5 justify-center bg-black bg-opacity-20 md:bg-opacity-15">
-            <div className="text-white font-bold text-lg">Electric Bike</div>
-            <button className="bg-white w-20 h-10 rounded-lg font-bold text-sm mt-2 md:w-24 md:h-12">Shop</button>
+            <div></div>
         </div>
-    </div>
-</div> */}
+        
  
     </>
    )
