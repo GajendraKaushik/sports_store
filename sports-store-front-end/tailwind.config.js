84aab4dcad8f28d272f3242bf331abf18fe1931d
GajendraKaushik <gajendrakaushik128@gmail.com>
2024-05-31 19:18:47 +0530

    Tailwind : Addded new css class for shadow

--- sports-store-front-end/tailwind.config.js
+++ sports-store-front-end/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '3xl': '-5px 10px 15px 6px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);',
+      }
+    },
   },
   plugins: [],
 }
